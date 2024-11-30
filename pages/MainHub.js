dd23c7e0f3240a810bae2bdb5bb710a0e4f57978
k1mmari <kwuffle133@gmail.com>
2024-11-29 21:10:46 -0800

    created a MainHub.js placeholder

---
+++ pages/MainHub.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const MainHub = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Todo Details Page</Text>
+      <Button
+        title="Back to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MainHub;
