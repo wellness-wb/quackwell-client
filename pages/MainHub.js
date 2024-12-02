ff224987875769a31a74fdcf758f985821e0b876
k1mmari <kwuffle133@gmail.com>
2024-12-02 09:54:57 -0800

    partially implemented mainhub with bottom menu bar and an empty upper menu bar

--- pages/MainHub.js
+++ pages/MainHub.js
@@ -1,23 +1,58 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ImageBackground, StyleSheet, TouchableOpacity, View } from "react-native";
+import MenuBar from "./MenuBar";
+import UpperMenu from "./UpperMenu";
 
 const MainHub = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>MainHub page</Text>
-      <Button
-        title="Back to Login"
-        onPress={() => navigation.navigate("Login")}
-      />
-    </View>
+    <ImageBackground
+      source={require("../assets/background.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+        <UpperMenu />
+        <DuckAnimation />
+        <MenuBar />
+    </ImageBackground>
   );
 };
 
+const DuckAnimation =() => {
+  const [isFirstImage, setIsFirstImage] = useState(true); // tracking which image is displayed
+  const handlePress = () => {
+    setIsFirstImage((prevState) => !prevState); // toggle the state
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handlePress}>
+        <Image
+        source={
+          isFirstImage
+          ? require("../assets/duckwithbodynohearts.png")
+          : require("../assets/duckwithbody.png")
+        }
+        style={styles.image}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'flex-bottom',
+  },
+  image: {
+    widtth: 300,
+    height: 300,
+    resizeMode: 'contain'
   },
 });
 
