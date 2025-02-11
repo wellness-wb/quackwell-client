cfe854ff8156fad0faa0fa0997ce063db74d52da
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-02-11 14:49:38 -0800

    [ADD] Responsive css - forgot password page

--- pages/user/ForgotPassword.js
+++ pages/user/ForgotPassword.js
@@ -5,11 +5,16 @@ import {
   ImageBackground,
   Keyboard,
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 import { sendPasswordResetLink } from "../../utils/auth";
 import { useBouncePress } from "../../utils/useBouncePress";
 import BubbleBackground from "./components/bubble/BubbleBackground";
@@ -20,8 +25,22 @@ import UserHeader from "./components/UserHeader";
 const ForgotPassword = ({ navigation }) => {
   const { bounceAnim, soundRef, handlePress } = useBouncePress();
   const [email, setEmail] = useState("");
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
 
   useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setIsKeyboardVisible(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setIsKeyboardVisible(false);
+      }
+    );
+
     const loadSound = async () => {
       const { sound } = await Audio.Sound.createAsync(
         require("../../assets/quack.mp3")
@@ -35,6 +54,9 @@ const ForgotPassword = ({ navigation }) => {
       if (soundRef.current) {
         soundRef.current.unloadAsync();
       }
+
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
     };
   }, [soundRef]);
 
@@ -72,91 +94,81 @@ const ForgotPassword = ({ navigation }) => {
     >
       <BubbleBackground />
 
-      {/* Header Message */}
-      <UserHeader title="Forgot Password" />
-
-      {/* Animation logo */}
-      <Pressable onPress={handlePress}>
-        <Animated.Image
-          source={require("../../assets/logo_animated.gif")}
-          style={[styles.gif.gifIcon, { transform: [{ scale: bounceAnim }] }]}
-          resizeMode="contain"
-        />
-      </Pressable>
-
       {/* Text field */}
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.form.container}>
-          <EditableInput
-            placeholder="Enter your email..."
-            value={email}
-            onChangeText={setEmail}
-            secureTextEntry={false}
-          />
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        keyboardShouldPersistTaps="handled"
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <UserHeader title="Forgot Password" />
+            <Pressable onPress={handlePress} style={styles.gifIcon}>
+              <Animated.Image
+                source={require("../../assets/logo_animated.gif")}
+                style={[styles.gifIcon, { transform: [{ scale: bounceAnim }] }]}
+              />
+            </Pressable>
+            <View style={styles.formContainer}>
+              <EditableInput
+                placeholder="Enter your email..."
+                value={email}
+                onChangeText={setEmail}
+                secureTextEntry={false}
+              />
 
-          <ContinueButton
-            title="Send Reset Email"
-            onPress={handleResetPassword}
-          />
-        </View>
-      </TouchableWithoutFeedback>
+              <ContinueButton
+                title="Send Reset Email"
+                onPress={handleResetPassword}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+
       {/* Back to Login link */}
-      <View style={styles.footer.container}>
-        <Text style={styles.footer.bottomText}>
-          Go back to{" "}
-          <Text
-            style={styles.footer.bottomLink}
-            onPress={() =>
-              navigation.reset({
-                index: 0,
-                routes: [{ name: "Login" }],
-              })
-            }
-          >
-            Log in
+      {!isKeyboardVisible && (
+        <View style={styles.footer.container}>
+          <Text style={styles.footer.bottomText}>
+            Go back to{" "}
+            <Text
+              style={styles.footer.bottomLink}
+              onPress={() =>
+                navigation.reset({
+                  index: 0,
+                  routes: [{ name: "Login" }],
+                })
+              }
+            >
+              Log in
+            </Text>
           </Text>
-        </Text>
-      </View>
+        </View>
+      )}
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
+    width: "100%",
+    height: "100%",
   },
-  header: {
-    position: "absolute",
-    top: 100,
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: hp("5%"),
+  },
+  gifIcon: {
+    height: hp("30%"),
+    aspectRatio: 1,
+  },
+  formContainer: {
     width: "100%",
     alignItems: "center",
-  },
-  headerText: {
-    color: "#153CE6",
-    fontSize: 50,
-    fontFamily: "Inter",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-
-  gif: {
-    gifIcon: {
-      width: "140%",
-      height: undefined,
-      aspectRatio: 1, // scaling GIF proportionally
-    },
-  },
-
-  form: {
-    container: {
-      position: "absolute",
-      top: "50%",
-      width: "100%",
-      alignItems: "center",
-      paddingHorizontal: 20,
-    },
+    flexGrow: 1,
   },
   footer: {
     container: {
@@ -167,12 +179,12 @@ const styles = StyleSheet.create({
     },
     bottomText: {
       textAlign: "center",
-      fontSize: 18,
+      fontSize: wp("4.2%"),
     },
     bottomLink: {
       color: "#153CE6",
       fontWeight: "bold",
-      fontSize: 18,
+      fontSize: wp("4.5%"),
     },
   },
 });
