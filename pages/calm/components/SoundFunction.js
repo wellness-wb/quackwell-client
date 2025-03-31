2940cb37ddb287ad2539e17b92c861c3e6652e0e
Kaung Khant <kaung.khant.673@my.csun.edu>
2025-03-31 14:46:58 -0700

    Change the assessbility of Sound Files

--- pages/calm/components/SoundFunction.js
+++ pages/calm/components/SoundFunction.js
@@ -1,28 +1,32 @@
-// Helper function that allows user to choose the sound option they prefer
-const loadAndPlaySound = async () => {
-  let soundFile;
-  // Here funcitonality that let the users choose the sound file based on user's choice
-  if (selectedSound === 'raining') {
-    soundFile = require('../../../assets/raining.mp3');
-  } else if (selectedSound === 'meditation') {
-    soundFile = require('../../../assets/suzumeChilling.mp3');
-  } else {
-    soundFile = require('../../../assets/raining.mp3'); //By default, it's raining sound
-  }
+import { Audio } from 'expo-av'; // Importing the Audio module from expo-av to handle audio playback
+import React from 'react'; // Importing React to use hooks and components
 
-  // Load the sound with looping enabled (if desired)
-  const playSound = async (soundFile) => {
-    try {
-      const { sound: playbackSound } = await Audio.Sound.createAsync(
-        soundFile,
-        { shouldPlay: true, isLooping: true },
-      );
-      setSound(playbackSound);
-      await playbackSound.playAsync();
-    } catch (error) {
-      console.error('Error playing sound!!!', error); // Handle any errors in loading or playing the sound
-    }
-  };
+// Function to map selected sound names to actual sound files
+const getSoundFile = (soundName) => {
+  switch (soundName) {
+    case 'Raining for power nap':
+      return require('../../../assets/raining.mp3');
+    case 'Suzume Soundtrack to Calm Yourself':
+      return require('../../../assets/peacefulOcean.mp3');
+    case 'Ocean Waves to reduce your stress':
+      return require('../../../assets/suzumeChilling.mp3');
+    default:
+      return require('../../../assets/raining.mp3'); // Default to rain sound
+  }
+};
+
+// Load the sound with looping enabled (if desired)
+const playSound = async (soundFiles) => {
+  try {
+    const { sound: playbackSound } = await Audio.Sound.createAsync(soundFiles, {
+      shouldPlay: true,
+      isLooping: true,
+    });
+    setSound(playbackSound);
+    await playbackSound.playAsync();
+  } catch (error) {
+    console.error('Error playing sound!!!', error); // Handle any errors in loading or playing the sound
+  }
 };
 
 // Ensures the function only works if the sound is currently loaded and playing, otherwise no sound is playing so function does nothing
@@ -33,3 +37,5 @@ const stopSound = async () => {
     setSound(null); // resets the sound state to null which means you make sure the app does not attempt the stopped or unloaded sound later on
   }
 };
+
+export default SoundFunction;
