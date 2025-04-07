8ca9bf934f42994904590c6978e4c2af4785b75b
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-04-07 14:25:35 -0700

    [FIXED] mainHub Routing

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ function AppStack() {
         cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
         cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
       }}
-      initialRouteName="FriendsPage"
+      initialRouteName="MainHub"
     >
       {/* Main */}
       <Stack.Screen name="MainHub" component={MainHub} />
